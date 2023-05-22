eda314351bbc68af89f9887a62378118c6b863a4
yuna.yabe <2023dounyukenshu.096@gmail.com>
2023-05-22 15:14:44 +0900

    バリデーションを追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
+import models.validators.MessageValidator;
 import utils.DBUtil;
 
 /**
@@ -49,6 +52,19 @@ public class UpdateServlet extends HttpServlet {
         Timestamp currentTime = new Timestamp(System.currentTimeMillis());
         m.setUpdated_at(currentTime);
 
+        List<String> errors = MessageValidator.validate(m);
+        if(errors.size() > 0) {
+            em.close();
+
+         // フォームに初期値を設定、さらにエラーメッセージを送る
+            request.setAttribute("_token", request.getSession().getId());
+            request.setAttribute("message", m);
+            request.setAttribute("errors", errors);
+
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+            rd.forward(request, response);
+        }else {
+
         em.getTransaction().begin();
         em.getTransaction().commit();
         request.getSession().setAttribute("flush", "更新が完了しました。");
@@ -57,6 +73,7 @@ public class UpdateServlet extends HttpServlet {
         request.getSession().removeAttribute("message_id");
 
         response.sendRedirect(request.getContextPath() + "/index");
+        }
 
 
 
